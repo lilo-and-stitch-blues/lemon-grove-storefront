6120158d1f0d1518c309aaf6762a0ff04609e559
Lemon Grove Dev <dev@lemongrove.example>
2026-09-01 15:39:39 +0000

    Wire up product loading in entrypoint

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 // Entry point for the Lemon Grove storefront service.
+const { PRODUCTS } = require("./products");
+
 console.log("Lemon Grove storefront booting...");
+console.log(`Loaded ${PRODUCTS.length} products.`);
